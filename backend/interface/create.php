e6375131d94b1efd08bf1ec0feae08ed0cd11fe8
Lukas Wanko <lwanko.mmt-m2014@fh-salzburg.ac.at>
2015-10-16 13:59:27 +0200

    merge conflict solved

--- backend/interface/create.php
+++ backend/interface/create.php
@@ -8,10 +8,6 @@
 
   for ($i = 0; $i < $count; $i++) {
     $human = Human::fromOverview($decoded[$i]["prename"], $decoded[$i]["lastname"], $decoded[$i]["department"], $decoded[$i]["type"], $decoded[$i]["id"]);
-<<<<<<< HEAD
     echo createHuman($human);
-=======
-    echo insertHuman($human);
->>>>>>> a79edd6e4a4d48eb02a7c334738a9a6e037ac922
   }
 
